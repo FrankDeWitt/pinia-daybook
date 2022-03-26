966cb6079c722816cac81461cfac1e133f1b24c7
Fran J Salvador <fs@thehotelsnetwork.com>
2022-03-26 14:19:49 +0100

    feat: font awesome autoimport include

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
 const path = require('path')
 
 // https://vitejs.dev/config/
@@ -9,7 +12,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        IconsResolver(),
+      ]
+    }),
+    Icons({ compiler: 'vue3' }),
+  ],
   server: {
     host: true,
   },
